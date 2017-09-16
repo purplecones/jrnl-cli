38dd18274a2497089dae668f40ce9c4277e35678
Mirza Joldic <mirza.joldic@gmail.com>
2017-09-16 18:48:43 -0400

    starting point

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import program from 'commander';
+import inquirer from 'inquirer';
+import { addEntry } from './localdb';
+
+program
+  .version('0.0.1')
+  .description('Tool for writing journal entries from the command line');
+
+program
+  .command('add')
+  .alias('a')
+  .description('Add new enty')
+  .action(() => {
+    var questions = [
+      {
+        type: 'editor',
+        name: 'entry',
+        message: 'Write your entry',
+        validate: function(text) {
+          addEntry(text);
+          return true;
+        },
+      },
+    ];
+
+    inquirer.prompt(questions).then(function(answers) {
+      console.log(JSON.stringify(answers, null, '  '));
+    });
+  });
+
+program.parse(process.argv);
